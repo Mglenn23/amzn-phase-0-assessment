6419a3a2813cd0f8e37316e5cd9dd44754df2f1f
michael <michaelglenn88@yahoo.com>
2022-04-04 22:43:38 -0400

    Completed Assignment

--- deli-counter.js
+++ deli-counter.js
@@ -1,24 +1,72 @@
 // 1. Write your functions here
+/**
+ * input : array,string
+ * output : string
+ * side-eff: array
+ */
+function takeANumber(arr, str) {
+  arr.push(str);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === str) {
+      console.log(`Welcome, ${str}. You are number ${[i + 1]} in line.`);
+    }
+  }
+}
 
+function nowServing(arr) {
+  if (arr.length > 0) {
+    for (let i = 0; i < arr.length; i++) {
+      console.log(`Currently serving ${arr.shift()}`);
+      break;
+    }
+  } else {
+    console.log("There is nobody waiting to be served!");
+  }
+}
+
+function line(arr) {
+  let newArr = [];
+  // console.log(arr)
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(i + 1 + ". " + arr[i]);
+  }
+  let stringReturn = newArr.toString().replaceAll(",", " ");
+  if (arr.length > 0) {
+    console.log(`The line is currently: ${stringReturn}`);
+  } else {
+    console.log("The line is currently empty.");
+  }
+}
 
 // 2. Example Usage
 
-// const katzDeli = []
+const katzDeli = [];
 
-// takeANumber(katzDeli, "Ada") //=> Welcome, Ada. You are number 1 in line.
-// takeANumber(katzDeli, "Grace") //=> Welcome, Grace. You are number 2 in line.
-// takeANumber(katzDeli, "Kent") //=> Welcome, Kent. You are number 3 in line.
+console.log(takeANumber(katzDeli, "Ada")); //=> Welcome, Ada. You are number 1 in line.
+console.log(takeANumber(katzDeli, "Grace")); //=> Welcome, Grace. You are number 2 in line.
+console.log(takeANumber(katzDeli, "Kent")); //=> Welcome, Kent. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+console.log(line(katzDeli)); //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-// nowServing(katzDeli) //=> "Currently serving Ada."
+console.log(nowServing(katzDeli)); //=> "Currently serving Ada."
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent"
+line(katzDeli); //=> "The line is currently: 1. Grace 2. Kent"
 
-// takeANumber(katzDeli, "Matz") //=> Welcome, Matz. You are number 3 in line.
+takeANumber(katzDeli, "Matz"); //=> Welcome, Matz. You are number 3 in line.
 
-// line(katzDeli) //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+line(katzDeli); //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
-// nowServing(katzDeli) //=> "Currently serving Grace."
+nowServing(katzDeli); //=> "Currently serving Grace."
 
-// line(katzDeli) //=> "The line is currently: 1. Kent 2. Matz"
+line(katzDeli); //=> "The line is currently: 1. Kent 2. Matz"
+
+//test running code
+// line(katzDeli);
+// takeANumber(katzDeli, "MG");
+// takeANumber(katzDeli, "Valen");
+//line(katzDeli);
+//   nowServing(katzDeli);
+// line(katzDeli);
+//  nowServing(katzDeli);
+//  nowServing(katzDeli);
+//  line(katzDeli);
