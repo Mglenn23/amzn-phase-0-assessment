35cf392c7c15d1abdf6c5a9f60cd8ceea7906030
michael <michaelglenn88@yahoo.com>
2022-04-12 00:31:06 -0400

    Completed Assignment Revision Cons to Return

--- deli-counter.js
+++ deli-counter.js
@@ -1,5 +1,5 @@
 // 1. Write your functions here
-
+// **Update Revision console.log to return line function.
 function takeANumber(arr, str) {
   arr.push(str);
   for (let i = 0; i < arr.length; i++) {
@@ -24,40 +24,42 @@ function line(arr) {
   }
   let stringReturn = newArr.toString().replaceAll(",", " ");
   if (arr.length > 0) {
-    console.log(`The line is currently: ${stringReturn}`);
+    //Before: console.log(`The line is currently: ${stringReturn}`)
+    return `The line is currently: ${stringReturn}`;
   } else {
-    console.log("The line is currently empty.");
+    //Before: console.log("The line is currently empty.")
+    return "The line is currently empty.";
   }
 }
 // 2. Example Usage
 
 const katzDeli = [];
 
-console.log(takeANumber(katzDeli, "Ada")); //=> Welcome, Ada. You are number 1 in line.
-console.log(takeANumber(katzDeli, "Grace")); //=> Welcome, Grace. You are number 2 in line.
-console.log(takeANumber(katzDeli, "Kent")); //=> Welcome, Kent. You are number 3 in line.
+takeANumber(katzDeli, "Ada"); //=> Welcome, Ada. You are number 1 in line.
+takeANumber(katzDeli, "Grace"); //=> Welcome, Grace. You are number 2 in line.
+takeANumber(katzDeli, "Kent"); //=> Welcome, Kent. You are number 3 in line.
 
 console.log(line(katzDeli)); //=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-console.log(nowServing(katzDeli)); //=> "Currently serving Ada."
+nowServing(katzDeli); //=> "Currently serving Ada."
 
-line(katzDeli); //=> "The line is currently: 1. Grace 2. Kent"
+console.log(line(katzDeli)); //=> "The line is currently: 1. Grace 2. Kent"
 
 takeANumber(katzDeli, "Matz"); //=> Welcome, Matz. You are number 3 in line.
 
-line(katzDeli); //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
+console.log(line(katzDeli)); //=> "The line is currently: 1. Grace 2. Kent 3. Matz"
 
 nowServing(katzDeli); //=> "Currently serving Grace."
 
-line(katzDeli); //=> "The line is currently: 1. Kent 2. Matz"
+console.log(line(katzDeli)); //=> "The line is currently: 1. Kent 2. Matz"
 
 //test running code
-// line(katzDeli);
+// console.log(line(katzDeli));
 // takeANumber(katzDeli, "MG");
 // takeANumber(katzDeli, "Valen");
-//  line(katzDeli);
-//   nowServing(katzDeli);
-// line(katzDeli);
-//  nowServing(katzDeli);
-//  nowServing(katzDeli);
-//  line(katzDeli);
+// console.log(line(katzDeli));
+// nowServing(katzDeli);
+// console.log(line(katzDeli));
+// nowServing(katzDeli);
+// nowServing(katzDeli);
+// console.log(line(katzDeli));
